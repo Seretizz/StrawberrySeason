570c37a2d5ebcba17951ff43230d70e9f36dd80a
Laura Bognanni <bognanni.laura@gmail.com>
2022-10-28 15:52:11 +0200

    modalviewP updated

--- StrawberrySeason/StrawberrySeason/Views/ModalViewP.swift
+++ StrawberrySeason/StrawberrySeason/Views/ModalViewP.swift
@@ -35,14 +35,17 @@ struct ModalViewP: View {
                     .fontWeight(.bold)
                     .padding()
                 
-                Text("Teens get acne for a number of reasons, but the most common reason is puberty. Your hormone levels are going through changes, which can lead to increased oil production in the skin’s pores and hair follicles.")
-                    .font(.title3)
-                    .frame(width: 350)
+                ScrollView{
+                    Text("Teens get acne for a number of reasons, but the most common reason is puberty. Your hormone levels are going through changes, which can lead to increased oil production in the skin’s pores and hair follicles. This increased oil, along with dead skin cells and bacteria, can clog your pores and cause acne. You might see acne in the form of whiteheads, blackheads, or pimples. Acne can also run in the family; if one or both of your parents dealt with acne growing up, you are likely to as well. Additionally, stress is another common source of acne in pre-teens and teenagers.")
+                        .font(.title)
+                        .frame(width: 350)
+                }
+                
                 
                 Image("BerryPimples")
                     .resizable()
-                    .frame(width: 200, height: 250)
-                    .offset(x: -80, y: -10)
+                    .frame(width: 150, height: 200)
+                    .offset(x: -80, y: -20)
                 
                 Spacer()
             }
